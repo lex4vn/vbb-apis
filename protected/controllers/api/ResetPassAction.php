5868bba334b5b26e99638bfd3e5546275fcf1c16
Nguyen Duc Tho <lex4vn@gmail.com>
2016-10-17 00:20:12 +0700

    Reset pass

--- protected/controllers/api/ResetPassAction.php
+++ protected/controllers/api/ResetPassAction.php
@@ -5,7 +5,7 @@ class ResetPassAction extends CAction
     public function run()
     {
         header('Content-type: application/json');
-        $email = isset($_POST['email']) ? $_POST['email'] : null;
+        $email = isset($_GET['email']) ? $_GET['email'] : null;
         if ($email == null) {
             echo json_encode(array('code' => 5, 'message' => 'Missing params email'));
             return;
@@ -25,38 +25,32 @@ class ResetPassAction extends CAction
             'platformversion' => PLATFORM_VERSION,
             'uniqueid' => $uniqueId]);
 
-        // var_dump($response);die(1);
-
         // Get token key
         $accessToken = $response['apiaccesstoken'];
 
         $apiConfig->setAccessToken($accessToken);
         $api = new Api($apiConfig, $apiConnector);
-//vb_login_password - The password of the User. If client use this to login, the plain password may be sniffed during the pass in the network
-//vb_login_md5password - The md5 password of the User
-//vb_login_md5password_utf - The md5 password (Unicode) of the User
-// logintype - Possible value: 'cplogin' or empty. 'cplogin' means that the login will also allow the user to access the AdminCP if they have permission.
         $response = $api->callRequest('login_emailpassword', [
             'email' => $email,
         ]);
-     //   var_dump($response);die(1);
-        //Thanh cong
         if (isset($response['response'])) {
             if (isset($response['response']->errormessage)) {
                 $result = $response['response']->errormessage[0];
                 if (!empty($response['response']->errormessage) && 'invalidemail' == $response['response']->errormessage[0]) {
-                    echo json_encode(array('code' => 2,'message' => 'Wrong email.'));
+                    echo json_encode(array('code' => 2,
+                        'result' => false,
+                        'message' => 'Wrong email.'));
                     return;
                 }
                 if ('redirect_lostpw' == $response['response']->errormessage) {
-                    //var_dump($response);
                     echo json_encode(array('code' => 0,
+                        'result' => true,
                         'message' => 'Your username and details about how to reset your password have been sent to you by email.',
                     ));
                     return;
                 }
             }
-        }else{
+        } else {
             echo json_encode(array('code' => 1, 'message' => 'Forum error'));
             return;
         }
