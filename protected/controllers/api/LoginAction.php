844aa78d8bef1d22ea8cd635f0fa0804dde3fb16
Nguyen Duc Tho <lex4vn@gmail.com>
2016-10-16 22:01:59 +0700

    login password MD5

--- protected/controllers/api/LoginAction.php
+++ protected/controllers/api/LoginAction.php
@@ -4,13 +4,11 @@ class LoginAction extends CAction
 {
     public function run()
     {
-        //test
         header('Content-type: application/json');
         if(empty($_POST)) {
             $_POST = json_decode(file_get_contents('php://input'), true);
         }
         $params = $_POST;
-        var_dump($_POST);
         if (!isset($params['username']) || $params['username'] == '' ) {
             echo json_encode(array('code' => 5, 'message' => 'Missing params username'));
             return;
@@ -19,7 +17,6 @@ class LoginAction extends CAction
             echo json_encode(array('code' => 5, 'message' => 'Missing params password'));
             return;
         }
-        //Parameters
         $uniqueId = uniqid();
         $content = '';
 
@@ -33,65 +30,34 @@ class LoginAction extends CAction
             'platformname' => PLATFORM_NAME,
             'platformversion' => PLATFORM_VERSION,
             'uniqueid' => $uniqueId]);
-
-        // var_dump($response);die(1);
-
         // Get token key
         $accessToken = $response['apiaccesstoken'];
 
         $apiConfig->setAccessToken($accessToken);
         $api = new Api($apiConfig, $apiConnector);
-//vb_login_password - The password of the User. If client use this to login, the plain password may be sniffed during the pass in the network
-//vb_login_md5password - The md5 password of the User
-//vb_login_md5password_utf - The md5 password (Unicode) of the User
-// logintype - Possible value: 'cplogin' or empty. 'cplogin' means that the login will also allow the user to access the AdminCP if they have permission.
         $response = $api->callRequest('login_login', [
             'vb_login_username' => $params['username'],
-            'vb_login_password' => $params['password']
+            'vb_login_md5password' => $params['password']
         ]);
-       //  var_dump($response);die(1);
-        //Thanh cong
-        //array(2) { ["session"]=> object(stdClass)#43 (2) {
-        // ["dbsessionhash"]=> string(32) "4214b0a1f30fbebb95e57cc3c12a6c93" ["userid"]=> int(21342) }
-        // ["response"]=> object(stdClass)#50 (1) { ["errormessage"]=> array(2) { [0]=> string(14) "redirect_login" [1]=> string(5) "sWa10" } } }
-
-        // Sai pass
-        //array(2) { ["session"]=> object(stdClass)#43 (2) {
-        // ["dbsessionhash"]=> string(32) "cad6cb765af7f03c4b0edd364eb13359" ["userid"]=> string(1) "0" }
-        // ["response"]=> object(stdClass)#50 (1) { ["errormessage"]=> array(4) { [0]=> string(25) "badlogin_strikes_passthru" [1]=> string(18) "http://pkl.vn/test" [2]=> string(49) "s=cad6cb765af7f03c4b0edd364eb13359&api=1&" [3]=> int(1) } } }
-
-
-       // array(2) { ["session"]=> object(stdClass)#43 (2) {
-        //
-        // ["dbsessionhash"]=> string(32) "955fd073816ddf01059e87369ae90e42"
-        // ["userid"]=> string(1) "0" } ["response"]=> object(stdClass)#50 (1) { ["errormessage"]=> array(3) { [0]=> string(7) "strikes" [1]=> string(18) "http://pkl.vn/test" [2]=> string(49) "s=955fd073816ddf01059e87369ae90e42&api=1&" } } }
-        //Wrong username or password. You have used up your failed login quota! Please wait 15 minutes before trying again. Don't forget that the password is case sensitive. Forgotten your password? Click here!
-
-        var_dump($response);die(1);
-
-        // var_dump($response['response']);die(1);
-        //  if(!isset($response->response->errorlist)){
 
         if (isset($response['response'])) {
             if (isset($response['response']->errormessage)) {
                 $result = $response['response']->errormessage[0];
                 if ('badlogin_strikes_passthru' == $result) {
-                    //var_dump($response);
-                    echo json_encode(array('code' => 2,'message' => 'Wrong username or password.'));
+                    echo json_encode(array('code' => 2,'result' =>  false,'message' => 'Wrong username or password.'));
                     return;
                 }
                 if ('strikes' == $result) {
-                    //var_dump($response);
-                    echo json_encode(array('code' => 2,
+                    echo json_encode(array('code' => 2,'result' =>  false,
                         'message' => 'Wrong username or password. You have used up your failed login quota! Please wait 15 minutes before trying again. Don\'t forget that the password is case sensitive.',
                     ));
                     return;
                 }
                 if ('redirect_login' == $result &&  $response['session']->userid !== '0') {
-                    //var_dump($response);
                     echo json_encode(array('code' => 0,
                         'message' => 'Login successful',
                         'sessionhash' =>  $response['session']->dbsessionhash,
+                        'result' =>  true,
                         'userid' =>  $response['session']->userid,
                         'username' =>  $response['response']->errormessage[1],
                     ));
@@ -99,7 +65,7 @@ class LoginAction extends CAction
                 }
             }
         }else{
-            echo json_encode(array('code' => 1, 'message' => 'Forum error'));
+            echo json_encode(array('code' => 1, 'result' =>  false,'message' => 'Forum error'));
             return;
         }
 
