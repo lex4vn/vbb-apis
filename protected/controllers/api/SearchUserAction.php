f5466aac7579e023e8e4666fa634e2446c52310e
TRUNGHIEU\admin <hieuntse02172@gmail.com>
2016-12-01 10:59:14 +0700

    update api search user using 1 param username

--- protected/controllers/api/SearchUserAction.php
+++ protected/controllers/api/SearchUserAction.php
@@ -6,20 +6,14 @@ class SearchUserAction extends CAction
     {
         header('Content-type: application/json');
         $params = $_GET;
-        if (isset($params['email'])) {
-            if ($params['email'] == '') {
-                echo json_encode(array('code' => 5, 'message' => 'Missing params email'));
-                return;
-            }
-        } else if (isset($params['username'])) {
-            if ($params['username'] == '') {
-                echo json_encode(array('code' => 5, 'message' => 'Missing params username'));
-                return;
-            }
-        } else {
-            echo json_encode(array('code' => 5, 'message' => 'Missing params email or username'));
-            return;
-        }
+       
+        
+		if (!isset($params['username']) || $params['username'] == '') {
+			echo json_encode(array('code' => 5, 'message' => 'Missing params username'));
+			return;
+		}
+		$isEmail = filter_var($params['username'], FILTER_VALIDATE_EMAIL);
+		
         //Parameters
         $uniqueId = uniqid();
         $content = '';
@@ -40,9 +34,9 @@ class SearchUserAction extends CAction
 
         $apiConfig->setAccessToken($accessToken);
         $api = new Api($apiConfig, $apiConnector);
-        if (!empty($params['email'])) {
+        if ($isEmail) {
             $response = $api->callRequest('api_emailsearch', [
-                'fragment' => $params['email'],'api_v'=> '1'
+                'fragment' => $params['username'],'api_v'=> '1'
             ]);
 
             if (count($response) >= 3) {
@@ -57,28 +51,25 @@ class SearchUserAction extends CAction
                 echo json_encode(array('code' => 1, 'message' => 'Email is not registered.'));
                 return;
             }
-        } else if (!empty($params['username'])) {
-            $response = $api->callRequest('api_usersearch', [
-                'fragment' => $params['username'],'api_v'=> '1'
-            ]);
+        } 
+		$response = $api->callRequest('api_usersearch', [
+			'fragment' => $params['username'],'api_v'=> '1'
+		]);
 
-            if (count($response) >= 3) {
-                echo json_encode(array('code' => 0,
-                    'message' => 'Successful',
-                    'userid' => $response[0],
-                    'username' => $response[1],
-                    'email' => $response[2],
-                ));
-                return;
-            }else{
-                echo json_encode(array('code' => 1, 'message' => 'Username is not registered.'));
-                return;
-            }
+		if (count($response) >= 3) {
+			echo json_encode(array('code' => 0,
+				'message' => 'Successful',
+				'userid' => $response[0],
+				'username' => $response[1],
+				'email' => $response[2],
+			));
+			return;
+		}else{
+			echo json_encode(array('code' => 1, 'message' => 'Username is not registered.'));
+			return;
+		}
 
-        } else {
-            echo json_encode(array('code' => 5, 'message' => 'Missing params email or username'));
-            return;
-        }
+        
 
 
 
