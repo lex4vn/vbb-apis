829cc45f0d82b2a0b7aca189d0626abb1217be4d
Nguyen Duc Tho <lex4vn@gmail.com>
2016-12-12 20:17:07 +0700

    update image

--- protected/controllers/api/NewThreadAction.php
+++ protected/controllers/api/NewThreadAction.php
@@ -82,7 +82,7 @@ class NewThreadAction extends CAction
             'f' => $params['type'] == 1? 69:17,
             'api_v' => '1'
         ], ConnectorInterface::METHOD_POST);
-        var_dump($response);
+        //var_dump($response);
         if (isset($response['response']->errormessage)) {
             if ($response['response']->errormessage == 'redirect_postthanks') {
                 echo json_encode(array('code' => 0, 'message' => 'Post successfull.'));
