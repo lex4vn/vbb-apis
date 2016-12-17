9b05ee67dda5df8f4d5a42f39c26fd511f32b61b
ifindthanh <thanhx0kjt@hotmail.com>
2016-12-17 13:23:42 +0700

    test

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -44,9 +44,7 @@ class ApiController extends CController
             && $action->id != "detailComments"
             && $action->id != "detailPost"
             && $action->id != "uploadImages"
-            && $action->id != "listProfileBuddy"
             && $action->id != "logout"
-			&& $action->id != "profile"
         ) {
             //var_dump($_POST['sessionhash']);die();
             $sessionKey = isset($_POST['sessionhash']) ? $_POST['sessionhash'] : null;
