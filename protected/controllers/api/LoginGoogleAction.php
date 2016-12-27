466bb579874caa0d5843b6d9f5c0aaba11422ac0
Nguyen Duc Tho <lex4vn@gmail.com>
2016-12-27 17:11:49 +0700

    login social

--- protected/controllers/api/LoginGoogleAction.php
+++ protected/controllers/api/LoginGoogleAction.php
@@ -5,91 +5,100 @@ class LoginGoogleAction extends CAction
     public function run()
     {
         header('Content-type: application/json');
-        $accessToken = isset($_GET['accessToken']) ? $_GET['accessToken'] : null;
+        if (empty($_POST)) {
+            $_POST = json_decode(file_get_contents('php://input'), true);
+        }
+        $params = $_POST;
+        $accessToken = isset($params['accessToken']) ? $params['accessToken'] : null;
 
         if ($accessToken == null) {
             echo json_encode(array('code' => 5, 'message' => 'Missing params accessToken'));
             return;
         }
+        $email = isset($params['email']) ? $params['email'] : null;
+        if ($email == null) {
+            echo json_encode(array('code' => 5, 'message' => 'Missing params email'));
+            return;
+        }
         // Get user infomation
-        require_once 'src/Google_Client.php';
-        require_once 'src/contrib/Google_PlusService.php';
-        require_once 'src/contrib/Google_Oauth2Service.php';
-        $client = new Google_Client();
-        $client->setScopes(array('https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/plus.me'));
-        $client->setApprovalPrompt('auto');
-        $service = new Google_Oauth2Service($client);
-
+//        require_once 'src/Google_Client.php';
+//        require_once 'src/contrib/Google_PlusService.php';
+//        require_once 'src/contrib/Google_Oauth2Service.php';
+//
+//        $client = new Google_Client();
+//        $client->setScopes(array('https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/plus.me'));
+//        $client->setApprovalPrompt('auto');
+//
+//        $service = new Google_Oauth2Service($client);
         Yii::log("accessToken: $accessToken \n");
-        if (isset($accessToken)) {
-            $client->setAccessToken($accessToken);
-        }
-        if ($client->getAccessToken()) {
-            $userProfile = $service->userinfo->get();
-            $uid = isset($userProfile['id']) ? $userProfile['id'] : '';
-            $Glastname = isset($userProfile['family_name']) ? $userProfile['family_name'] : '';
-            $Gfirstname = isset($userProfile['given_name']) ? $userProfile['given_name'] : '';
-            $Gmail = isset($userProfile['email']) ? $userProfile['email'] : '';
-            $Gpicture = isset($userProfile['picture']) ? $userProfile['picture'] : '';
-        }
-        // Log user in
-        $checkUser = Subscriber::model()->findByAttributes(array('username' => $uid));
-        if (count($checkUser) > 0) {
-            $checkUser->firstname = $Gfirstname;
-            $checkUser->lastname = $Glastname;
-            $checkUser->password = 'Google';
-            $checkUser->email = $Gmail;
-            $checkUser->url_avatar = $Gpicture;
-            if (!$checkUser->save()) {
-                echo '<pre>';
-                print_r($checkUser->getErrors());
+
+//        if (isset($accessToken)) {
+//            $client->setAccessToken($accessToken);
+//        }
+
+//        if ($client->getAccessToken()) {
+//            $userProfile = $service->userinfo->get();
+//            $uid = isset($userProfile['id']) ? $userProfile['id'] : '';
+//            $Glastname = isset($userProfile['family_name']) ? $userProfile['family_name'] : '';
+//            $Gfirstname = isset($userProfile['given_name']) ? $userProfile['given_name'] : '';
+//            $Gmail = isset($userProfile['email']) ? $userProfile['email'] : '';
+//            $Gpicture = isset($userProfile['picture']) ? $userProfile['picture'] : '';
+//        }
+
+        // Init api forum
+        $uniqueId = uniqid();
+        $apiConfig = new ApiConfig(API_KEY, $uniqueId, CLIENT_NAME, CLIENT_VERSION, PLATFORM_NAME, PLATFORM_VERSION);
+        $apiConnector = new GuzzleProvider(API_URL);
+        $api = new Api($apiConfig, $apiConnector);
+
+        $response = $api->callRequest('api_init', [
+            'clientname' => CLIENT_NAME,
+            'clientversion' => CLIENT_VERSION,
+            'platformname' => PLATFORM_NAME,
+            'platformversion' => PLATFORM_VERSION,
+            'uniqueid' => $uniqueId
+        ]);
+        // Get token key
+        $accessToken = $response['apiaccesstoken'];
+
+        $apiConfig->setAccessToken($accessToken);
+        $api = new Api($apiConfig, $apiConnector);
+
+        $response = $api->callRequest('login_facebook', [
+            'fbuserid' => $email,
+            'api_v' => '4'
+        ], ConnectorInterface::METHOD_POST);
+        $error = false;
+
+        if (isset($response['response'])) {
+            if (isset($response['response']->errormessage)) {
+                $result = $response['response']->errormessage[0];
+                //var_dump($result);die();
+                if ('redirect_login' == $result && $response['session']->userid !== '0') {
+                    $sessionKey = CUtils::generateSessionKey($response['session']->userid, base64_encode(serialize($apiConfig)));
+                    echo json_encode(array('code' => 0,
+                        'message' => 'Login successful',
+                        'sessionhash' => $sessionKey,
+                        'result' => true,
+                        'userid' => $response['session']->userid,
+                        'username' => $response['response']->errormessage[1],
+                    ));
+                    return;
+                }
+
+                if ('badlogin_facebook' == $result) {
+                    echo json_encode(array('code' => 2, 'result' => false, 'message' => 'Please register with google account'));
+                    return;
+                }
+
+            } else {
+                $error = true;
             }
-            $loginFirst = 1;
-            $avata = $Gpicture;
-            $fcoin = $checkUser->fcoin;
-            $type = $checkUser->type;
-            $partnerID = $checkUser->partner_id;
-            Yii::app()->session['user_id'] = $checkUser->id;
-            $sessionKey = CUtils::generateSessionKey($checkUser->id);
-        } else {
-            $subs = new Subscriber();
-            $subs->firstname = $Gfirstname;
-            $subs->lastname = $Glastname;
-            $subs->username = $uid;
-            $subs->password = 'Google';
-            $subs->device_type = '';
-            $subs->phone_number = '';
-            $subs->email = $Gmail;
-            $subs->type = 1;
-            $subs->status = 0;
-            $subs->fcoin = 0;
-            $subs->url_avatar = $Gpicture;
-            $subs->create_date = date('Y-m-d H:i:s');
-            if (!$subs->save()) {
-                echo '<pre>';
-                print_r($subs->getErrors());
-            }
-            Yii::app()->session['user_id'] = $subs->id;
-            $loginFirst = 0;
-            $type = $subs->type;
-            $avata = $subs->url_avatar;
-            $sessionKey = CUtils::generateSessionKey($subs->id);
         }
-        Yii::app()->session['session_key'] = $sessionKey;
-        echo json_encode(array(
-            'code' => 0,
-            'sessionkey' => $sessionKey,
-            'item' => array(
-                'id' => Yii::app()->session['user_id'],
-                'username' => !empty($uid) ? $uid : 'Noname',
-                'url_avatar' => $avata,
-                'lastname' => !empty($Glastname) ? $Glastname : '',
-                'firstname' => !empty($Gfirstname) ? $Gfirstname : '',
-                'fcoin' => $fcoin,
-                'loginFirst' => $loginFirst,
-                'type' => !empty($type) ? $type : '',
-                'versionApp' => '1.0'
-            ),
-        ));
+
+        if ($error) {
+            echo json_encode(array('code' => 1, 'result' => false, 'message' => 'Forum error'));
+            return;
+        }
     }
 }
