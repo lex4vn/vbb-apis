8af56645ac2a15b92e32aa66371cb963512a3ac6
Nguyen Duc Tho <lex4vn@gmail.com>
2017-01-10 19:59:01 +0700

    update history

--- protected/controllers/api/MessagesHistoryAction.php
+++ protected/controllers/api/MessagesHistoryAction.php
@@ -24,32 +24,61 @@ class MessagesHistoryAction extends CAction
             }
 
             $html = $response['response']->HTML;
-            if ($html->foderid == 0) {
+//            var_dump($html);
+//            die();
+            if ($html->folderid == 0) {
+
+                $messages = array();
+                $groups = $html->messagelist_periodgroups;
+               // var_dump($groups);die();
+                foreach ($groups as $group) {
+                    $messagesBits = $group->messagelistbits;
+                    $numberMessage = $group->messagesingroup;
+                    if($numberMessage > 1) {
+                        foreach ($messagesBits as $messagesBit) {
+
+                            //                        if(isset($messagesBit->pm)){
+                            //                            continue;
+                            //                        }
+
+                            $messages[] = array(
+                                'messageid' => $messagesBit->pm->pmid,
+                                'sender_id' => $messagesBit->userbit[0]->userid,
+                                'sender_name' => $messagesBit->userbit[0]->username,
+                                'senddate' => date('m/d/Y h:i:s', strtotime($messagesBit->pm->senddate . ' ' . $messagesBit->pm->sendtime)),
+                                'statusicon' => $messagesBit->pm->statusicon,
+                                'receipt_id' => Yii::app()->session['user_id'],
+                                'content' => $messagesBit->pm->title,
+                                'avatar_url' => '',
+                            );
+                            //                        var_dump($messages);
+                            //                        die();
+                        }
+                    }else{
+                        $messages[] = array(
+                            'messageid' => $messagesBits->pm->pmid,
+                            'sender_id' => $messagesBits->userbit[0]->userid,
+                            'sender_name' => $messagesBits->userbit[0]->username,
+                            'senddate' => date('m/d/Y h:i:s', strtotime($messagesBits->pm->senddate . ' ' . $messagesBits->pm->sendtime)),
+                            'statusicon' => $messagesBits->pm->statusicon,
+                            'receipt_id' => Yii::app()->session['user_id'],
+                            'content' => $messagesBits->pm->title,
+                            'avatar_url' => '',
+                        );
+                    }
+                }
+
+//                                        var_dump($messages);
+//                        die();
+                echo json_encode(array('code' => 0,
+                    'message' => 'Get history messages successful',
+                    'listmessages' => $messages
+                ));
+                return;
+            }else{
                 echo json_encode(array('code' => 10, 'message' => 'You have no messages.'));
                 return;
             }
-            $messages = array();
-            $groups = $html->messagelist_periodgroups;
-            foreach ($groups as $group) {
-                $messagesBits = $group->messagelistbits;
-                foreach ($messagesBits as $messagesBit) {
-                    $messages[] = array(
-                        'messageid' => $messagesBit->pm->pmid,
-                        'sender_id' => $messagesBit->userbit->userid,
-                        'sender_name' => $messagesBit->userbit->username,
-                        'senddate' => date('d/m/Y h:i:s', strtotime($messagesBit->pm->senddate . ' ' . $messagesBit->pm->sendtime)),
-                        'statusicon' => $messagesBit->pm->statusicon,
-                        'receipt_id' => Yii::app()->session['user_id'],
-                        'content' => $messagesBit->pm->title,
-                        'avatar_url' => '',
-                    );
-                }
-            }
-            echo json_encode(array('code' => 0,
-                'message' => 'Get history messages successful',
-                'listmessages' => $messages
-            ));
-            return;
         } else {
             echo json_encode(array('code' => 10, 'message' => 'User logged out'));
             return;
