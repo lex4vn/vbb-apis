03ede60382362b77e9602ceac453c3af3c68348f
Nguyen Duc Tho <lex4vn@gmail.com>
2017-01-14 22:31:42 +0700

    using sessionhash

--- protected/controllers/api/DetailCommentAction.php
+++ protected/controllers/api/DetailCommentAction.php
@@ -15,59 +15,41 @@ class DetailCommentAction extends CAction
         if ($pageNumber == null) {
             $pageNumber = 1;
         }
-        $userId = isset($_GET['userid']) ? $_GET['userid'] : null;
-        if ($userId == null) {
-            echo json_encode(array('code' => 5, 'message' => 'you need login'));
-            return;
-        }
-        //Parameters
-        $uniqueId = uniqid();
-        $content = '';
 
-        $apiConfig = new ApiConfig(API_KEY, $uniqueId, CLIENT_NAME, CLIENT_VERSION, PLATFORM_NAME, PLATFORM_VERSION);
-        $apiConnector = new GuzzleProvider(API_URL);
-        $api = new Api($apiConfig, $apiConnector);
+        $sessionhash = CUtils::getSessionHash($params['sessionhash']);
+        if ($sessionhash) {
+            $apiConfig = unserialize(base64_decode($sessionhash));
+            $api = new Api($apiConfig, new GuzzleProvider(API_URL));
 
-        $response = $api->callRequest('api_init', [
-            'clientname' => CLIENT_NAME,
-            'clientversion' => CLIENT_VERSION,
-            'platformname' => PLATFORM_NAME,
-            'platformversion' => PLATFORM_VERSION,
-            'uniqueid' => $uniqueId]);
+            $response = $api->callRequest('showthread', [
+                'threadid' => $threadId, 'api_v'=> '1', 'pagenumber' => $pageNumber
+            ], ConnectorInterface::METHOD_GET);
 
-        // var_dump($response);die(1);
-
-        // Get token key
-        $accessToken = $response['apiaccesstoken'];
-
-        $apiConfig->setAccessToken($accessToken);
-        $api = new Api($apiConfig, $apiConnector);
-
-        $response = $api->callRequest('showthread', [
-            'threadid' => $threadId, 'api_v'=> '1', 'pagenumber' => $pageNumber
-        ], ConnectorInterface::METHOD_GET);
-
-        if (isset($response['response'])) {
-            $result = array();
-            foreach ( $response["response"]->postbits as $postbit){
-                $item = array(
-                    'username' => $postbit->post->username,
-                    'avatarurl' => $postbit->post->avatarurl,
-                    'onlinestatus' => $postbit->post->onlinestatus,
-                    'usertitle' => $postbit->post->usertitle,
-                    'message' => $postbit->post->message,
-                    'postdate' => $postbit->post->postdate,
-                    'isMycomment' => $postbit->post->userid == $userId
-                );
-                array_push($result, $item);
+            if (isset($response['response'])) {
+                $result = array();
+                foreach ( $response["response"]->postbits as $postbit){
+                    $item = array(
+                        'username' => $postbit->post->username,
+                        'avatarurl' => $postbit->post->avatarurl,
+                        'onlinestatus' => $postbit->post->onlinestatus,
+                        'usertitle' => $postbit->post->usertitle,
+                        'message' => $postbit->post->message,
+                        'postdate' => $postbit->post->postdate,
+                        'isMycomment' => $postbit->post->userid ==  Yii::app()->session['user_id']
+                    );
+                    array_push($result, $item);
+                }
+                echo json_encode(array('code' => 0,
+                    'message' => 'get detail comment success',
+                    'listcomment' => $result
+                ));
+                return;
+            } else {
+                echo json_encode(array('code' => 1, 'message' => 'Forum error'));
+                return;
             }
-            echo json_encode(array('code' => 0,
-                'message' => 'get detail comment success',
-                'listcomment' => $result
-            ));
-            return;
         } else {
-            echo json_encode(array('code' => 1, 'message' => 'Forum error'));
+            echo json_encode(array('code' => 2, 'message' => 'User logged out'));
             return;
         }
 // con thieu phan lay email, SĐT, check user hiện tại có phải là người đăng post hay là ban hay ko
