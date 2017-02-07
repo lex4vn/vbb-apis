53b0b973bd1c2d53e3a280cee6a822c4dcf37142
ifindthanh <thanhx0kjt@hotmail.com>
2017-02-07 11:07:42 +0700

    detail friend

--- protected/controllers/api/DetailFriendAction.php
+++ protected/controllers/api/DetailFriendAction.php
@@ -9,26 +9,21 @@ class DetailFriendAction extends CAction
             $_POST = json_decode(file_get_contents('php://input'), true);
         }
         $params = $_POST;
-       // var_dump($_POST['sessionhash']);die();
-		$userid = isset($params['userid']) ? $params['userid'] : null;
-		if ($userid == null) {
+		if (!isset($params['userid']) || $params['userid'] == '') {
 			echo json_encode(array('code' => 5, 'message' => 'Missing params userid'));
 			return;
-		}
+		} 
         $sessionhash = CUtils::getSessionHash(($params['sessionhash']));
-		//var_dump($sessionhash);die();
         if ($sessionhash) {
             $apiConfig = unserialize(base64_decode($sessionhash));
             $api = new Api($apiConfig, new GuzzleProvider(API_URL));
-
 			$profile = $api->callRequest('member ', [
-				'userid'=> $userid,
+			    'userid'=> $params['userid'],
 				'api_v' => '1'
 			], ConnectorInterface::METHOD_POST);
 
-			// TODO update
-			var_dump($profile);die();
-			if(isset($profile['response'])) {
+            if(isset($profile['response'])){
+				$result = array();			
 				$fields = $profile['response']->blocks->aboutme->block_data->fields->category->fields;
 				$phonenumber = '';
 				$fullname = '';
@@ -39,19 +34,25 @@ class DetailFriendAction extends CAction
 						$phonenumber = $field->profilefield->value;
 					}
 				}
+				
 				$item = array(
-					'userid' => $user->userid,
-					'username' => $user->username,
+					'username' => $profile['response']->prepared->username,
 					'avatarurl' => $profile['response']->prepared->avatarurl,
 					'fullname' => $fullname,
 					'phonenumber' => $phonenumber,
 					'onlinestatus' => $profile['response']->prepared->onlinestatus->onlinestatus == 1 ? "online" : "offline",
 					'displayemail' => $profile['response']->prepared->displayemail,
-				);
+					);
 				array_push($result, $item);
-			}
-
-			//var_dump($profile);die();
+               
+				echo json_encode(array('code' => 0,
+					'message' => 'Get profile of friend success',
+					'listfriend' => $result
+				));
+            }else{
+                echo json_encode(array('code' => 2, 'message' => 'Forum error'));
+                return;
+            }
         }
         else {
             // Sessionhash is empty
