c52a99ecfd810950126854de677a1312ff1adcd5
Nguyen Duc Tho <lex4vn@gmail.com>
2017-02-20 20:20:42 +0700

    login face

---
+++ protected/models/Comment.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * This is the model class for table "comment".
+ *
+ * The followings are the available columns in table 'comment':
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $post_id
+ * @property string $content
+ * @property string $create_date
+ * @property string $modify_date
+ * @property integer $count_like
+ * @property integer $count_reply
+ * @property integer $status
+ */
+class Comment extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'comment';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id, post_id, content', 'required'),
+			array('user_id, post_id, count_like, count_reply, status', 'numerical', 'integerOnly'=>true),
+			array('create_date, modify_date', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, user_id, post_id, content, create_date, modify_date, count_like, count_reply, status', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'user_id' => 'User',
+			'post_id' => 'Post',
+			'content' => 'Content',
+			'create_date' => 'Create Date',
+			'modify_date' => 'Modify Date',
+			'count_like' => 'Count Like',
+			'count_reply' => 'Count Reply',
+			'status' => 'Status',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('post_id',$this->post_id);
+		$criteria->compare('content',$this->content,true);
+		$criteria->compare('create_date',$this->create_date,true);
+		$criteria->compare('modify_date',$this->modify_date,true);
+		$criteria->compare('count_like',$this->count_like);
+		$criteria->compare('count_reply',$this->count_reply);
+		$criteria->compare('status',$this->status);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Comment the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
