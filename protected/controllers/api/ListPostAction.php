659bfe366727e104eb826cf9d94e5e34eae7cfb9
Nguyen Duc Tho <lex4vn@gmail.com>
2017-03-11 20:47:43 +0700

    sort by date

--- protected/controllers/api/ListPostAction.php
+++ protected/controllers/api/ListPostAction.php
@@ -43,6 +43,7 @@ class ListPostAction extends CAction
                 //->order('t.modify_date desc')
                 ->limit($limit)
                 ->offset($offset)
+                ->order('modify_date desc')
                 ->queryAll();
 
 
