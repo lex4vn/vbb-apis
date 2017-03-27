25bcf0883ac7a1616f567aa9d9d4a8ecf9beb687
Nguyen Duc Tho <lex4vn@gmail.com>
2017-03-27 08:47:51 +0700

    login face

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -160,11 +160,13 @@ where p.type = $type order by p.modify_date desc limit $offset, $page_size";
             $this->redirect(Yii::app()->homeurl);
         }
         $post = Post::model()->findByPk($id);
+        Yii::log('Bai viet'.$id);
         if ($post == null) {
             $this->redirect(Yii::app()->homeurl);
         }
 
         $user_id = Yii::app()->session['user_id'];
+        Yii::log('User id'.$user_id);
         // 22671 thond  // ksoft 20364
         if($user_id != 22671 || $user_id != 20364){
             $this->redirect(Yii::app()->homeurl);
