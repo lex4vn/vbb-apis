d2f33dc2a6dc951e12ba85c2617b8ea2280a5391
Nguyen Duc Tho <lex4vn@gmail.com>
2017-04-01 16:24:59 +0700

    login face

--- protected/controllers/api/FriendAddAction.php
+++ protected/controllers/api/FriendAddAction.php
@@ -26,8 +26,67 @@ class FriendAddAction extends CAction
         $user = User::model()->findByPk($params['userid']);
 
         if ($user == null) {
-            echo json_encode(array('code' => 1, 'message' => 'Can not add this user.'));
-            return;
+            $isError = false;
+            if ($sessionhash) {
+                $apiConfig = unserialize(base64_decode($sessionhash));
+                $api = new Api($apiConfig, new GuzzleProvider(API_URL));
+                $profile = $api->callRequest('member ', [
+                    'userid'=> $params['userid'],
+                    'api_v' => '1'
+                ], ConnectorInterface::METHOD_POST);
+
+                if(isset($profile['response'])){
+                    $result = array();
+                    $fields = $profile['response']->blocks->aboutme->block_data->fields->category->fields;
+                    $phonenumber = '';
+                    $fullname = '';
+                    foreach($fields as $field) {
+                        if ($field->profilefield->title == "Họ và Tên") {
+                            $fullname = $field->profilefield->value;
+                        } else if ($field->profilefield->title == "Phone Number"){
+                            $phonenumber = $field->profilefield->value;
+                        }
+                    }
+                    $usertitle = '';
+                    $avatar = '';
+                    $status = '';
+                    $email = '';
+                    if(isset($profile['response']->prepared)) {
+                        $usertitle = $profile['response']->prepared->usertitle;
+                        $avatar = str_replace("amp;","",API_URL.$profile['response']->prepared->avatarurl);
+                        $status = $profile['response']->prepared->onlinestatus->onlinestatus == 1 ? "1" : "0";
+                        $email = $profile['response']->prepared->displayemail;
+                    }
+
+                    $user = User::model()->findByPk($params['userid']);
+                    if($user == null){
+                        $user = new User();
+                        $user->userid = $params['userid'];
+                        $user->username = $profile['response']->prepared->username;
+                        $user->phonenumber =  $phonenumber;
+                        $user->password =  $profile['response']->prepared->displayemail;
+                        $user->usertitle = $fullname;
+                        $user->avatar = $avatar;
+                        $user->status = $status;
+                    }else{
+                        $user->phonenumber =  $phonenumber;
+                        $user->usertitle = $usertitle;
+                        //$user->avatar = $avatar;
+                        $user->password =  $email;
+                        $user->status = $status;
+                    }
+                    $user->save();
+
+                }else{
+                    $isError = true;
+                }
+            }
+
+            if($isError){
+                echo json_encode(array('code' => 1, 'message' => 'Can not add this user.'));
+                return;
+            }
+
         }
 
         if ($userId) {
