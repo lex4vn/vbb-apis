e54daa778378155aaa9acbd4d2b2c03728cfffa4
Nguyen Duc Tho <lex4vn@gmail.com>
2017-05-04 20:25:05 +0700

    login face

--- protected/models/Chat.php
+++ protected/models/Chat.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
- * This is the model class for table "chat".
+ * This is the model class for table "api_chat".
  *
- * The followings are the available columns in table 'chat':
+ * The followings are the available columns in table 'api_chat':
  * @property integer $id
  * @property integer $fromid
  * @property string $fromuser
+ * @property integer $status_from
  * @property integer $to
  * @property string $touser
+ * @property integer $status_to
  * @property string $message
  * @property integer $read
  * @property string $time
@@ -32,11 +34,11 @@ class Chat extends CActiveRecord
 		// will receive user inputs.
 		return array(
 			array('fromid, fromuser, to, touser, message, read, time', 'required'),
-			array('fromid, to, read', 'numerical', 'integerOnly'=>true),
+			array('fromid, status_from, to, status_to, read', 'numerical', 'integerOnly'=>true),
 			array('fromuser, touser', 'length', 'max'=>50),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, fromid, fromuser, to, touser, message, read, time', 'safe', 'on'=>'search'),
+			array('id, fromid, fromuser, status_from, to, touser, status_to, message, read, time', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -58,10 +60,12 @@ class Chat extends CActiveRecord
 	{
 		return array(
 			'id' => 'ID',
-			'fromid' => 'From',
+			'fromid' => 'Fromid',
 			'fromuser' => 'Fromuser',
+			'status_from' => 'Status From',
 			'to' => 'To',
 			'touser' => 'Touser',
+			'status_to' => 'Status To',
 			'message' => 'Message',
 			'read' => 'Read',
 			'time' => 'Time',
@@ -87,10 +91,12 @@ class Chat extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('id',$this->id);
-		$criteria->compare('fromid',$this->from);
+		$criteria->compare('fromid',$this->fromid);
 		$criteria->compare('fromuser',$this->fromuser,true);
+		$criteria->compare('status_from',$this->status_from);
 		$criteria->compare('to',$this->to);
 		$criteria->compare('touser',$this->touser,true);
+		$criteria->compare('status_to',$this->status_to);
 		$criteria->compare('message',$this->message,true);
 		$criteria->compare('read',$this->read);
 		$criteria->compare('time',$this->time,true);
